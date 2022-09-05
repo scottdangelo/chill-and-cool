15fcb8448d7e8df2b169300a296ffff8570db6a0
Scott DAngelo <scott.dangelo@gmail.com>
2022-09-05 17:15:59 -0600

    Change to static and add trailing slash

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const express = require("express");
 
 const app = express();
 
+/*
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
-  });
-//app.use(express.static('../Dylan-website'))
+  //})
+  */
+app.use(express.static('../Dylan-website/'))
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
